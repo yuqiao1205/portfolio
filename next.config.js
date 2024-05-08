cacca988ac95ddc9f0bea0a7277b021f1fec58ff
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-07 18:33:05 -0700

    Add assetPrefix to work around base path doesn't work

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
+const isProd = (process.env.NODE_ENV === 'production')
+
+const nextConfig = {  // Use the CDN in production and localhost for development.
+    assetPrefix: isProd ? 'https://yuqiao1205.github.io/portfolio/' : undefined
+}
+ 
 module.exports = nextConfig
