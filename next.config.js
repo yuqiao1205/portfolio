597eb28de107ba397ffbfe6adb5d9316a1026a60
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-07 18:55:34 -0700

    Remove all workaround

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = (process.env.NODE_ENV === 'development')
-
-const nextConfig = {  // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://yuqiao1205.github.io/portfolio/' : undefined,
+const nextConfig = {
     images: { unoptimized: true },
 }
  
