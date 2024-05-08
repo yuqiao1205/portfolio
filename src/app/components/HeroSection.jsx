876da3adacb77de5142404d854a1d38bccaff708
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-08 14:11:21 -0700

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -51,7 +51,7 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="#contact"
               // className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
             >
