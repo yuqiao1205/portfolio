a1b57fdfdb2fa886f69b475f5fab584865452d89
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-07 18:52:02 -0700

    Try to use dev workaround

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = (process.env.NODE_ENV === 'production')
+const isProd = (process.env.NODE_ENV === 'development')
 
 const nextConfig = {  // Use the CDN in production and localhost for development.
     assetPrefix: isProd ? 'https://yuqiao1205.github.io/portfolio/' : undefined,
-    images: { unoptimized: true},
+    images: { unoptimized: true },
 }
  
 module.exports = nextConfig
