1e94ec7f145c2617af806549f85147f9e4fe87fe
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-07 17:28:33 -0700

    add path

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -71,7 +71,7 @@ const HeroSection = () => {
             </a> */}
 
             <Link
-              href="/images/yan_resume_pink.pdf"
+              href="/portfolio/images/yan_resume_pink.pdf"
               download
               // href="/images/yan_resume_pink1.1.pdf"
               // to={resume}
@@ -92,7 +92,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#334155] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/py5.png"
+              src="/portfolio/images/py5.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
               // className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
