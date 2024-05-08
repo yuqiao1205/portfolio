fd0303c1bc96dd69900a34b312160850e06d064b
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-07 18:42:22 -0700

    Use NODE_ENV as production in github workflow

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@
 const isProd = (process.env.NODE_ENV === 'production')
 
 const nextConfig = {  // Use the CDN in production and localhost for development.
-    assetPrefix: isProd ? 'https://yuqiao1205.github.io/portfolio/' : undefined
+    assetPrefix: isProd ? 'https://yuqiao1205.github.io/portfolio/' : undefined,
+    images: { unoptimized: true},
 }
  
 module.exports = nextConfig
