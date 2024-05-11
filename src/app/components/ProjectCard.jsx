4456502a862f4e5ec5b05e219617fd85fb54cfba
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-05-10 18:42:15 -0700

    add link to works URL

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-// import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
+import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { CodeBracketIcon } from "@heroicons/react/24/solid";
+// import { CodeBracketIcon } from "@heroicons/react/24/solid";
 
-
-const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
+const ProjectCard = ({
+  imgUrl,
+  title,
+  description,
+  gitUrl,
+  previewUrl,
+  worksUrl,
+}) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group overflow-hidden"
-        style={{ background: `url(${imgUrl})`,  backgroundSize: "contain",  backgroundRepeat: "no-repeat", backgroundPosition: "center"  }}
+        style={{
+          background: `url(${imgUrl})`,
+          backgroundSize: "contain",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+        }}
       >
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
@@ -18,16 +29,49 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           >
             <CodeBracketIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
+          {previewUrl ? (
+            <Link
+              href={previewUrl}
+              className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            >
+              <EyeIcon className="h-14 w-14 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            </Link>
+          ) : (
+            <div className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] group">
+              <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#ADB7BE] text-sm">
+                Soon
+              </span>
+            </div>
+          )}
+
+          {/* {previewUrl !== "" ? (
+            <Link
+              href={previewUrl}
+              className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            >
+              <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
+            </Link>
+          ) : (
+            <div className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] group">
+              <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+            </div>
+          )} */}
+
           {/* <Link
             href={previewUrl}
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link> */}
-        </div> 
+        </div>
       </div>
       <div className="rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className=" text-xl font-semibold mb-2">{title}</h5>
+        <h4 className="text-xl font-semibold mb-2">
+          <Link href={worksUrl} className="text-sm underline text-pink-400">
+            {worksUrl}
+          </Link>
+        </h4>
         <p className="text-[#94a3b8]">{description}</p>
       </div>
     </div>
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,13 +7,14 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
-    title: "TravelWise - React Web Application for Travel Enthusiasts.",
+    title: "TravelWise - Using React for the frontend, with a Node.js backend and MySQL database deploy to Azure.",
     description:
       " The blog app not only provides traditional blog features but also offers users with a unique experience by integrating cutting-edge features such as an AI-powered travel chatbot for personalized recommendations, AI-powered vision feature for destination identification and assistance, automated question answering for seamless information retrieval, and real-time weather updates with counrtry info checks to enhance trip planning.",
     image: "images/projects/travelwise.jpg",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/blogproject",
-    previewUrl: "/",
+    previewUrl: "https://tp2024.westus3.cloudapp.azure.com/",
+    worksUrl: "https://tp2024.westus3.cloudapp.azure.com/",
   },
   {
     id: 2,
@@ -24,20 +25,34 @@ const projectsData = [
     image: "images/projects/imagepost.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/photo-site",
-    previewUrl: "/",
+    previewUrl: "",
+    worksUrl: "",
   },
   {
     id: 3,
+    title:
+      "Movie Review - Nextjs with IMDB API, responsive design and a dark mode switch.",
+    description:
+      "The Next.js project uses a 3rd party IMDB API to fetch movie data and enable searching, creating a seamless interface. It includes a section for top-rated and trending movies, offering curated recommendations. The app's responsive design provides a comprehensive movie review platform, allowing users to explore detailed information, ratings, and reviews. Additionally, the app features a dark mode option for enhanced viewing comfort in low-light environments.",
+    image: "images/projects/moviereview-dark.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/yuqiao1205/movie-review",
+    previewUrl: "https://movie-review-kappa-ten.vercel.app/",
+    worksUrl: "https://movie-review-kappa-ten.vercel.app/",
+  },
+  {
+    id: 4,
     title: "Banking System - Java Application for Banking System.",
     description:
       "The project encompasses the implementation of a banking system featuring checking and savings accounts, as well as cash deposit functionality. It allows users to create various types of accounts, reset account details, view account information, and perform withdrawals. The project leverages JavaFX to create a user-friendly graphical interface for input, enhancing the overall user experience ",
     image: "images/projects/banksystem.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/advancedjava/tree/main/assigment1/BankingSystem/src",
-    previewUrl: "/",
+    previewUrl: "",
+    worksUrl: "",
   },
   {
-    id: 4,
+    id: 5,
     title:
       "Brick Game Application - Javascript Based Application using HTML, CSS, and JS.",
     description:
@@ -45,20 +60,22 @@ const projectsData = [
     image: "images/projects/brickgame2.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/brickgame",
-    previewUrl: "/",
+    previewUrl: "https://brickgame-two.vercel.app/",
+    worksUrl: "https://brickgame-two.vercel.app/",
   },
   {
-    id: 5,
+    id: 6,
     title: "Calculator Application - Java Based Application.",
     description:
       "The project involves implementing an infix expression evaluator using Java, with a focus on essential mathematical functions and utilizing two stacks. Additionally, a Java Swing GUI is created to provide a user-friendly interface for calculator input. This approach allows users to input complex mathematical expressions in infix notation, which are then evaluated accurately. The project enhances user experience by providing a visually appealing and intuitive way to perform calculations. ",
     image: "images/projects/calculator1.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/csc413-ex-1-calculator",
-    previewUrl: "/",
+    previewUrl: "",
+    worksUrl: "",
   },
   {
-    id: 6,
+    id: 7,
     title:
       "User Management System - Spring Boot Application with MySQL Database and React Frontend.",
     description:
@@ -66,17 +83,19 @@ const projectsData = [
     image: "images/projects/usersystem.png",
     tag: ["All", "Web"],
     gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "",
+    worksUrl: "",
   },
   {
-    id: 7,
+    id: 8,
     title: "Handwritten Digit Recognition - Deep Learning Model using Pytorch.",
     description:
       "The project involves developing and evaluating a Convolutional Neural Network (CNN) model for classifying handwritten digits from the well-known MNIST dataset. The CNN is trained to accurately recognize and classify the digits, showcasing its ability to effectively handle image classification tasks.",
     image: "images/projects/handwritting1.png",
     tag: ["All", "Deep Learning"],
     gitUrl: "https://github.com/yuqiao1205/handwriting-app",
-    previewUrl: "/",
+    previewUrl: "",
+    worksUrl: "",
   },
 ];
 
@@ -136,6 +155,7 @@ const ProjectsSection = () => {
               imgUrl={project.image}
               gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
+              worksUrl={project.worksUrl}
             />
           </motion.li>
         ))}
