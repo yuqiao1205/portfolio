2ddebd7262c767206aa9a2a699eb87f38101ad53
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-09-11 13:50:55 -0700

    update projecttag page

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -8,17 +8,25 @@ const TAB_DATA = [
     title: "Skills",
     id: "skills",
     content: (
-      <ul className="flex flex-wrap">
-  <li className="w-1/3">Java</li>
-  <li className="w-1/3">Node.js</li>
-  <li className="w-1/3">Express</li>
-  <li className="w-1/3">React</li>
-  <li className="w-1/3">JavaScript</li>
-  <li className="w-1/3">HTML/CSS</li>
-  <li className="w-1/3">MySQL</li>
-  <li className="w-1/3">Python</li>
-  <li className="w-1/3">C++</li>
-</ul>
+      <ul className="grid grid-cols-3 gap-4 ">
+        <li className="w-1/4">Java</li>
+        <li className="w-1/4">Node.js</li>
+        <li className="w-1/4">Express</li>
+        <li className="w-1/4">React</li>
+        <li className="w-1/4">NextJs</li>
+        <li className="w-1/4">ReactNative</li>
+        <li className="w-1/4 ">JavaScript</li>
+
+        <li className="w-1/4">HTML/CSS</li>
+
+        <li className="w-1/4">Python</li>
+        <li className="w-1/4">PyTorch</li>
+        <li className="w-1/4">C++</li>
+
+        <li className="w-1/4">SpringBoot</li>
+        <li className="w-1/4">MySQL</li>
+        <li className="w-1/4">MongoDB</li>
+      </ul>
     ),
   },
   {
@@ -28,7 +36,7 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>Bachelor’ s degree: Computer Science</li>
         <li>San Francisco State University</li>
-        <li style={{ textDecoration: 'underline' }}>GPA:4.0</li>
+        <li style={{ textDecoration: "underline" }}>GPA:4.0</li>
       </ul>
     ),
   },
@@ -50,7 +58,7 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>
           <Link href="/images/solid_ppt.pdf">
-            SOLID Principles  Presentation 
+            SOLID Principles Presentation
           </Link>
         </li>
         <li>
@@ -95,14 +103,15 @@ const AboutSection = () => {
             month, where I have honed my skills in a variety of technologies,
             including Java, Node.js, React.js, Next.js, C++, Python, database
             management, cloud infrastructure, and web development.
-          </p><br></br>
+          </p>
+          <br></br>
           <p className="text-base lg:text-lg">
-            One of my notable achievements is the development of a
-            comprehensive travel website, where I single-handedly designed and
-            implemented entire the frontend, backend, REST API, database
-            architecture, and managed its deployment on the Azure cloud server.
-            In addition, I integrated advanced functionalities into the website
-            by leveraging the OpenAI API for text generation and vision. 
+            One of my notable achievements is the development of a comprehensive
+            travel website, where I single-handedly designed and implemented
+            entire the frontend, backend, REST API, database architecture, and
+            managed its deployment on the Azure cloud server. In addition, I
+            integrated advanced functionalities into the website by leveraging
+            the OpenAI API for text generation and vision.
           </p>
           <br></br>
 
@@ -136,7 +145,7 @@ const AboutSection = () => {
               {" "}
               Certifications{" "}
             </TabButton>
-       
+
             <TabButton
               selectTab={() => handleTabChange("ppts")}
               active={tab === "ppt"}
--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { useTheme } from 'next-themes';
-
+import { useTheme } from "next-themes";
 
 const ProjectTag = ({ name, onClick, isSelected }) => {
-
-const { theme } = useTheme();
-const isDarkMode = theme === 'dark';
+  const { theme } = useTheme();
+  const isDarkMode = theme === "dark";
 
   const buttonStyles = isSelected
-    ? "border-primary-500": isDarkMode? "text-[#ADB7BE] border-slate-600 hover:border-white"
+    ? "border-primary-500"
+    : isDarkMode
+    ? "text-[#ADB7BE] border-slate-600 hover:border-white"
     : "text-[#ADB7BE] border-slate-600 hover:border-[#a5f3fc]";
   return (
     <button
