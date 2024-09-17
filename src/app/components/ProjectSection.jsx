8a40298d22401b2a5a6b6bfd5ac272ea64528f10
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2024-09-16 19:20:21 -0700

    add calculator and imagepost

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -92,7 +92,13 @@ const projectsData = [
       "Photo Gallery - Nodejs, Express, Javascript, MySQL, Handlebar, CSS, HTML.",
     description:
       "The project implements a range of features catering to both unregistered and registered users, including login, register, post images, search, and make comments functionalities. Unregistered users can browse and search for content, while registered users gain additional capabilities such as posting images and making comments. The system offers a seamless and engaging experience, allowing users to interact with the platform's content in a dynamic and user-friendly manner.",
-    imgUrls: ["images/projects/imagepost.png"],
+    imgUrls: [
+      "images/projects/imagepost/imagepost-homepage.png",
+      "images/projects/imagepost/imagepost-loginpage.png",
+      "images/projects/imagepost/imagepost-signup.png",
+      "images/projects/imagepost/imagepost-addpost.png",
+      "images/projects/imagepost/imagepost-single.png",
+    ],
     // image: "images/projects/imagepost.png",
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/photo-site",
@@ -148,7 +154,11 @@ const projectsData = [
     title: "Calculator Application - Java, Java Swing GUI.",
     description:
       "The project involves implementing an infix expression evaluator using Java, with a focus on essential mathematical functions and utilizing two stacks. Additionally, a Java Swing GUI is created to provide a user-friendly interface for calculator input. This approach allows users to input complex mathematical expressions in infix notation, which are then evaluated accurately. The project enhances user experience by providing a visually appealing and intuitive way to perform calculations. ",
-    image: "images/projects/calculator1.png",
+    imgUrls: [
+      "images/projects/calculator/calexample01.png",
+      "images/projects/calculator/calexample02.png",
+      "images/projects/calculator/calculator.png",
+    ],
     tag: ["All", "Web"],
     gitUrl: "https://github.com/yuqiao1205/csc413-ex-1-calculator",
     previewUrl: "",
@@ -159,7 +169,7 @@ const projectsData = [
     title: "User Management System - Spring Boot, MySQL, React.",
     description:
       "This Spring Boot application manages users with CRUD operations, utilizing a MySQL database and a React frontend. It enables creating, reading, updating, and deleting user accounts, ensuring efficient user data management. The React frontend provides a user-friendly interface for seamless interaction with the system.",
-    image: "images/projects/usersystem.png",
+    imgUrls: ["images/projects/usersystem.png"],
     tag: ["All", "Web"],
     gitUrl: "/",
     previewUrl: "",
@@ -170,7 +180,7 @@ const projectsData = [
     title: "Handwritten Digit Recognition - Deep Learning Model using Pytorch.",
     description:
       "The project involves developing and evaluating a Convolutional Neural Network (CNN) model for classifying handwritten digits from the well-known MNIST dataset. The CNN is trained to accurately recognize and classify the digits, showcasing its ability to effectively handle image classification tasks.",
-    image: "images/projects/handwritting1.png",
+    imgUrls: ["images/projects/handwritting1.png"],
     tag: ["All", "DL"],
     gitUrl: "https://github.com/yuqiao1205/handwriting-app",
     previewUrl: "",
@@ -200,7 +210,7 @@ const projectsData = [
     title: "MovieMate Mobile App - React Native, Expo, TMDB.",
     description:
       "MovieMate is a mobile app built with React Native that keeps you updated with the latest movies using the TMDB API. Discover what’s currently showing in theaters and get a sneak peek at upcoming releases. With a clean, user-friendly interface, MovieMate provides movie details, ratings, and trailers, making it your go-to app for all things cinema.",
-    image: "images/projects/movieapp7.png",
+    imgUrls: ["images/projects/movie_mobileapp/movie_review.png"],
     tag: ["All", "Mobile"],
     gitUrl: "https://github.com/yuqiao1205/movie-mobile-app",
     previewUrl:
