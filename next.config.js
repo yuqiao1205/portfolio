23fd2154ae14cccda489c178f02badff24be03d1
Yan Peng <89482727+yuqiao1205@users.noreply.github.com>
2025-11-25 14:35:57 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     images: { unoptimized: true },
-    basePath: isProd ? '/portfolio' : '',
-    assetPrefix: isProd ? '/portfolio/' : '',
+    basePath: '',
+    assetPrefix: '',
 }
 
 module.exports = nextConfig
